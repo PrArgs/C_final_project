e3fbb834105b8d832eac7f12e83f75143cc7723b
Roi Argaman <zebroi.arg@gmail.com>
2023-08-25 20:34:41 +0300

    Update pre_assembler.c

--- pre_assembler.c
+++ pre_assembler.c
@@ -23,11 +23,12 @@ bool pre_assembler(char *file_name, macro_table *table){
     strcpy(line, "");
     strcpy(error_msg, "");
 
-    printf("file name: %s\n am file name %s\n", file_name, file_name_am);
+    
 
 
     file = fopen(file_name, "r");
     if(!file){
+        printf("file name: %s\n am file name %s\n", file_name, file_name_am);
         printf("Error: file %s does not exist\n", file_name);
         free(macro_name);
         free(error_msg);
